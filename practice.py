0ab078a977dca9e5baeda2a1cc68b5de6f281f5b
JRA2002 <105953543+JRA2002@users.noreply.github.com>
2024-05-25 18:58:04 +0200

    add exe

---
+++ practice.py
@@ -0,0 +1,11 @@
+# Listas de diferente longitud
+list1 = [1, 2]
+list2 = ['a', 'b', 'c']
+
+# Usar zip
+combined = zip(list1, list2)
+
+# Convertir a una lista para ver el resultado
+combined_list = list(combined)
+print(combined_list)
+
